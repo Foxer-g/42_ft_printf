8414c430adcac6051da3ac29b600f74c1072536c
Tom Espinosa <toespino@2F6.42angouleme.fr>
2025-11-10 20:33:35 +0100

    keskecéksetmerde

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: toespino <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/02 14:21:14 by toespino          #+#    #+#             */
-/*   Updated: 2025/11/10 19:38:54 by toespino         ###   ########.fr       */
+/*   Updated: 2025/11/10 20:00:29 by toespino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../ft_printf.h"
@@ -17,19 +17,19 @@ static int	ft_misc(char *str, va_list arg)
 
 	out = 1;
 	if (*str == 'c')
-		out = ft_putchar_f(va_arg(arg, int));
+		out += ft_putchar_f(va_arg(arg, int));
 	else if (*str == 's')
-		out = ft_putstr_f(va_arg(arg, char *));
+		out += ft_putstr_f(va_arg(arg, char *));
 	else if (*str == 'X' || *str == 'x')
-		out = ft_puthexa_f(va_arg(arg, int), *str == 'X');
+		out += ft_puthexa_f(va_arg(arg, int), *str == 'X');
 	else if (*str == 'd' || *str == 'i')
-		out = ft_putnbr_f(va_arg(arg, int));
+		out += ft_putnbr_f(va_arg(arg, int));
 	else if (*str == 'u')
-		out = ft_putunbr_f(va_arg(arg, unsigned int));
+		out += ft_putunbr_f(va_arg(arg, unsigned int));
 	else if (*str == 'p')
-		out = ft_puthexa_f(va_arg(arg, int), 0);
+		out += ft_puthexa_f(va_arg(arg, int), 0);
 	else
-		str--;
+		out += ft_putchar_f(*str);	
 	str++;
 	return (out);
 }
